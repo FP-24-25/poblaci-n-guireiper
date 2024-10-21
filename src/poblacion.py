52c805411a0476bb6d4e7c01e5d53471a8c15579
sdj2674 <guireiper@us.es>
2024-10-21 17:13:48 +0200

    proyecto terminado

---
+++ src/poblacion.py
@@ -0,0 +1,67 @@
+from collections import namedtuple
+import csv
+
+RegistroPoblacion = namedtuple('RegistroPoblacion', 'pais, codigo, año, censo')
+
+def lee_poblaciones(ruta):
+    res = []
+
+    with open(ruta, encoding='UTF-8') as f:
+        lista = csv.reader(f)
+        for pais, codigo, año, censo in lista :
+            año = int(año)
+            censo = int(censo)
+            tupla = RegistroPoblacion(pais, codigo, año, censo)
+            res.append(tupla)
+        return res
+    
+
+def calcula_paises(poblaciones):
+    res = set()
+    
+    for tupla in poblaciones:
+        res.add(tupla.pais)
+
+    res=list(res)
+    res.sort()
+    return res
+
+def filtra_por_pais(poblaciones, nombre_o_codigo):
+    res = []
+
+    for linea in poblaciones:
+        if nombre_o_codigo == linea.pais or nombre_o_codigo == linea.codigo:
+            tupla = (nombre_o_codigo, linea.año, linea.censo)
+            res.append(tupla)
+    return res
+
+def filtra_por_paises_y_anyo(poblaciones, anyo, paises):
+    res = []
+
+    for linea in poblaciones:
+        if anyo == linea.año and linea.pais in paises:
+            tupla = (linea.pais, linea.censo)
+            res.append(tupla)
+    return res
+
+def muestra_evolucion_poblacion(poblaciones, nombre_o_codigo):
+    lista_años = []
+    lista_habitantes = []
+
+    for linea in poblaciones:
+        if nombre_o_codigo == linea.pais or nombre_o_codigo == linea.codigo:
+            lista_años.append(linea.año)
+            lista_habitantes.append(linea.censo)
+
+    return lista_años, lista_habitantes
+
+def muestra_comparativa_paises_anyo(poblaciones, anyo, paises):
+    lista_habitantes = []
+    lista_paises = []
+
+    for linea in poblaciones:
+        if anyo == linea.año and linea.pais in paises:
+            lista_paises.append(linea.pais)
+            lista_habitantes.append(linea.censo)
+
+    return lista_paises, lista_habitantes
---
+++ src/poblacion_test.py
@@ -0,0 +1,70 @@
+from poblacion import *
+import matplotlib.pyplot as plt
+
+def test_lee_poblaciones(datos):
+    print(' ---- Lista de registro de poblacion ----')
+    print('Los primeros tres elementos')
+    for elementos in datos[:3]:
+        print(elementos)
+    
+    print('Los ultimos tres elementos')
+    for elementos in datos[-3:]:
+        print(elementos)
+
+def test_calcula_paises(lista):
+    print('Lista de paises para los que hay datos')
+    print(f'Hay un total de {len(lista)} paises registrados')
+    print(lista)
+
+
+def test_filtra_por_pais(lista, nombre):
+    print(f'Cuando se manda {nombre} se recoge esto:')
+    print('Los primeros tres elementos')
+    for elementos in lista[:3]:
+        print(elementos)
+    
+    print('Los ultimos tres elementos')
+    for elementos in lista[-3:]:
+        print(elementos)
+
+def test_filtra_por_paises_y_anyo(lista, año, listanombres):
+    print('En estos paises',listanombres,'en el año',año,'su censo era:')
+
+    for elementos in lista:
+        print(elementos)
+
+def test_muestra_evolucion_poblacion(lista_años, lista_habitantes, nombre):
+    titulo = f'Evolucion de poblacion de {nombre}'
+
+    plt.title(titulo)
+    plt.plot(lista_años, lista_habitantes)
+    plt.show()
+
+
+def test_muestra_comparativa_paises_anyo(lista_paises, lista_habitantes,año, listanombres):
+    titulo = f'Comparacion de poblacion en el año {año} de los paises {listanombres}'
+
+    plt.title(titulo)
+    plt.bar(lista_paises, lista_habitantes)
+    plt.show()
+    
+
+
+
+
+if __name__=="__main__":
+    datos = lee_poblaciones('data/population.csv')
+    #test_lee_poblaciones(datos) 
+    #calculo = calcula_paises(datos)   
+    #test_calcula_paises(calculo)
+    #nombrepais = 'ESP'
+    #filtrado = filtra_por_pais(datos, nombrepais)
+    #test_filtra_por_pais(filtrado, nombrepais)
+    listanombres = ['Tanzania','Micronesia','Mexico']
+    año = 2000
+    #listanyo = filtra_por_paises_y_anyo(datos, año, listanombres)
+    #test_filtra_por_paises_y_anyo(listanyo, año, listanombres)
+    #lista_años, lista_habitantes = muestra_evolucion_poblacion(datos, nombrepais)
+    #test_muestra_evolucion_poblacion(lista_años, lista_habitantes, nombrepais)
+    lista_paises2, lista_habitantes2 = muestra_comparativa_paises_anyo(datos, año, listanombres)
+    test_muestra_comparativa_paises_anyo(lista_paises2, lista_habitantes2,año, listanombres)
